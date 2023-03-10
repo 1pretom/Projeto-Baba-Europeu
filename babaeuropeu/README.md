ea53b71998ee6e3574443ed1e64a49a9e877d6e8
Washington <washingtonldamacenac@gmail.com>
2023-03-09 22:11:24 -0300

    mudança de pasta

--- babaeuropeu/README.md
+++
@@ -1,2 +0,0 @@
-# Projeto Baba Europeu
- Projeto para o baba europeu
---
+++ sitebabaeuropeu/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+/* import Navbar from './Components/NavBar' */
+import "./global.css"
+import "./Styles/main.css"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    
+    <App />
+    
+
+  </React.StrictMode>,
+)
